b5efe50156b94882837459fadd9b63477f3ea208
oksherlock <oksherlock9661@gmail.com>
2025-12-23 15:06:18 +0100

    0.2 - HomePage added

---
+++ ecommerce-project/src/components/HomePage.jsx
@@ -0,0 +1,9 @@
+import Body from "./HomePage/Body";
+import Header from "./HomePage/Header";
+
+export default function HomePage() {
+    return (<>
+        <Header />
+        <Body />
+    </>)
+}
