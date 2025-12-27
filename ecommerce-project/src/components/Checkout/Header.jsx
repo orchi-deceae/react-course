f02985117d57d8bc5204a74a671926185c145f1c
oksherlock <oksherlock9661@gmail.com>
2025-12-27 12:52:22 +0100

    0.10.1 - Update links to match the router

--- ecommerce-project/src/components/Checkout/Header.jsx
+++ ecommerce-project/src/components/Checkout/Header.jsx
@@ -1,11 +1,11 @@
-export default function Header(){
+export default function Header() {
     return (<>
         <title>Checkout</title>
 
         <div className="checkout-header">
             <div className="header-content">
                 <div className="checkout-header-left-section">
-                    <a href="index.html">
+                    <a href="/">
                         <img className="logo" src="images/logo.png" />
                         <img className="mobile-logo" src="images/mobile-logo.png" />
                     </a>
@@ -13,7 +13,7 @@ export default function Header(){
 
                 <div className="checkout-header-middle-section">
                     Checkout (<a className="return-to-home-link"
-                        href="index.html">3 items</a>)
+                        href="/">3 items</a>)
                 </div>
 
                 <div className="checkout-header-right-section">
