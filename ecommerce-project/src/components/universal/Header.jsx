1a21d24b75969e15902bb6df2f36e56ef30703e4
oksherlock <oksherlock9661@gmail.com>
2025-12-30 19:51:07 +0100

    0.16 - css changes

--- ecommerce-project/src/components/universal/Header.jsx
+++ ecommerce-project/src/components/universal/Header.jsx
@@ -1,5 +1,5 @@
 import cart from "../../backend/cart.json"
-import { Link } from "react-router"
+import { NavLink } from "react-router"
 
 export default function Header() {
     return (<>
@@ -7,10 +7,10 @@ export default function Header() {
 
         <div className="header">
             <div className="left-section">
-                <Link to="/" className="header-link">
+                <NavLink to="/" className="header-link">
                     <img className="logo" src="images/logo-white.png" />
                     <img className="mobile-logo" src="images/mobile-logo-white.png" />
-                </Link>
+                </NavLink>
             </div>
 
             <div className="middle-section">
@@ -22,16 +22,16 @@ export default function Header() {
             </div>
 
             <div className="right-section">
-                <Link className="orders-link header-link" to="/orders">
+                <NavLink className="orders-link header-link" to="/orders">
 
                     <span className="orders-text">Orders</span>
-                </Link>
+                </NavLink>
 
-                <Link className="cart-link header-link" to="/checkout">
+                <NavLink className="cart-link header-link" to="/checkout">
                     <img className="cart-icon" src="images/icons/cart-icon.png" />
                     <div className="cart-quantity">{cart.length}</div>
                     <div className="cart-text">Cart</div>
-                </Link>
+                </NavLink>
             </div>
         </div>
     </>)
