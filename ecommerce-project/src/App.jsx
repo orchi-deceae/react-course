8e83c0cb30ef2d1ff9fd7577c8b0504c9773a043
oksherlock <oksherlock9661@gmail.com>
2025-12-31 01:22:33 +0100

    0.22 - Not found page created

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Checkout from './pages/Checkout';
 import Orders from './pages/Orders';
 import Tracking from './pages/Tracking';
+import NotFound from './pages/NotFound';
 
 function App() {
     return (<>
@@ -11,6 +12,7 @@ function App() {
             <Route path='/checkout' element={<Checkout />} />
             <Route path='/orders' element={<Orders />} />
             <Route path='/tracking' element={<Tracking />} />
+            <Route path='*' element={<NotFound />} />
         </Routes>
     </>)
 }
---
+++ ecommerce-project/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import Header from "../components/universal/Header";
+import "../components/Home/Page"
+
+export default function NotFound(){
+    return (<>
+        <Header />
+        <p>Page not found</p>
+    </>)
+}
