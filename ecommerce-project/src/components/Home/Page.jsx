555c6cf40a74c63f343274c7a07abd24d0c73f15
oksherlock <oksherlock9661@gmail.com>
2026-01-01 15:16:45 +0100

    0.26 - Use data from the backend to generate the html

--- ecommerce-project/src/components/Home/Page.jsx
+++ ecommerce-project/src/components/Home/Page.jsx
@@ -1,14 +1,18 @@
-import products from "../../backend/products.json"
+import { useEffect, useState } from "react"
 import ProductGrid from "./extra/ProductGrid"
 import axios from 'axios'
 
 
 export default function Page(){
-    async function getProducts(){
-        const response = await axios.get("http://localhost:3000/api/products")
-        console.log(response.data)
-    }
-    getProducts()
+    const [products, setProducts] = useState([])
+    useEffect(()=>{
+        async function loadProducts(){
+            const response = await axios.get("http://localhost:3000/api/products")
+            console.log(response.data)
+            setProducts(response.data)
+        }
+        loadProducts()
+    }, [])
     return(<>
         <link rel="icon" type="image/svg+xml" href="images/home-favicon.png" />
         
