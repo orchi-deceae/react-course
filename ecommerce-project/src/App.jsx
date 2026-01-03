bdef9ebc6ec50f37a7c8018716f0d7426d28b59c
oksherlock <oksherlock9661@gmail.com>
2026-01-03 09:56:26 +0100

    0.30 - Lift cart up and share it between pages and copied favicons into backend

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router';
+import { useEffect, useState } from "react"
+import axios from 'axios'
 import Home from './pages/Home';
 import Checkout from './pages/Checkout';
 import Orders from './pages/Orders';
@@ -6,13 +8,29 @@ import Tracking from './pages/Tracking';
 import NotFound from './pages/NotFound';
 
 function App() {
+    const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
+    useEffect(()=>{
+        async function loadProducts(){
+            const response = await axios.get("/api/products")
+            console.log(response.data)
+            setProducts(response.data)
+        }
+        async function loadCart(){
+            const response = await axios.get("/api/cart-items")
+            setCart(response.data)
+        }
+        loadProducts()
+        loadCart()
+    }, [])
+
     return (<>
         <Routes>
-            <Route index element={<Home />} />
-            <Route path='/checkout' element={<Checkout />} />
-            <Route path='/orders' element={<Orders />} />
-            <Route path='/tracking' element={<Tracking />} />
-            <Route path='*' element={<NotFound />} />
+            <Route index element={<Home cart={cart} products={products} />} />
+            <Route path='/checkout' element={<Checkout cart={cart} products={products} />} />
+            <Route path='/orders' element={<Orders cart={cart} products={products} />} />
+            <Route path='/tracking' element={<Tracking cart={cart} products={products} />} />
+            <Route path='*' element={<NotFound cart={cart} products={products} />} />
         </Routes>
     </>)
 }
--- ecommerce-project/src/pages/Home.jsx
+++ ecommerce-project/src/pages/Home.jsx
@@ -1,28 +1,10 @@
 import Header from "../components/universal/Header";
 import Page from "../components/Home/Page";
-import { useEffect, useState } from "react"
-import axios from 'axios'
 import "../components/universal/general.css"
 import "../components/universal/Header.css"
 import "../components/Home/Page.css"
 
-export default function Home() {
-    const [products, setProducts] = useState([])
-    const [cart, setCart] = useState([])
-    useEffect(()=>{
-        async function loadProducts(){
-            const response = await axios.get("/api/products")
-            console.log(response.data)
-            setProducts(response.data)
-        }
-        async function loadCart(){
-            const response = await axios.get("/api/cart-items")
-            setCart(response.data)
-        }
-        loadProducts()
-        loadCart()
-    }, [])
-
+export default function Home({ cart, products }) {
     return (<>
         <Header cart={cart}/>
         <Page products={products}/>
