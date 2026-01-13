7ebe50df366fcf24e234831faf2cf6adbadd9b20
oksherlock <oksherlock9661@gmail.com>
2026-01-13 14:32:36 +0100

    0.45.2e - added update functionality to checkout

--- ecommerce-project/src/components/Checkout/extra/CartItemContainer.jsx
+++ ecommerce-project/src/components/Checkout/extra/CartItemContainer.jsx
@@ -2,13 +2,29 @@ import formatCurrency from '../../utils/formatCurrency'
 import DeliveryOption from './_DeliveryOption';
 import DeliveryDate from './_DeliveryDate';
 import axios from 'axios';
+import { useState } from 'react';
 
 export default function CartItemContainer({ cartItem, loadCart, deliveryOptions }) {
+    const [isUpdated, setIsUpdated] = useState(false)
+    const [quantity, setQuantity] = useState(cartItem.quantity)
     async function deleteCartItem(){
         await axios.delete(`/api/cart-items/${cartItem.productId}`)
         loadCart()
     }
-
+    function update() {
+        axios.put(`/api/cart-items/${cartItem.productId}`, {quantity})
+        setIsUpdated(false)
+        loadCart()
+    }
+    function specialEvents(ev){
+        console.log(typeof ev.key)
+        if (ev.key === "Enter") update()
+        if (ev.key === "Escape") {
+            setQuantity(cartItem.quantity)
+            setIsUpdated(false)
+        }
+    }
+    
     return Boolean(deliveryOptions.length) && (<>
         <div className="cart-item-container">
             <DeliveryDate 
@@ -29,9 +45,22 @@ export default function CartItemContainer({ cartItem, loadCart, deliveryOptions 
                     </div>
                     <div className="product-quantity">
                         <span>
-                            Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+                            Quantity: 
+                            {isUpdated ? 
+                                (<input 
+                                    type="text" 
+                                    value={quantity}
+                                    className="quantity-input-"
+                                    onKeyDown={specialEvents}
+                                    onChange={(ev)=>{setQuantity(Number(ev.target.value))}}
+                                />)
+                                :
+                                (<span className="quantity-label">{cartItem.quantity}</span>)
+                            }
                         </span>
-                        <span className="update-quantity-link link-primary">
+                        <span className="update-quantity-link link-primary"
+                            onClick={()=>!isUpdated ? setIsUpdated(true) : update()}
+                        >
                             Update
                         </span>
                         <span className="delete-quantity-link link-primary"
