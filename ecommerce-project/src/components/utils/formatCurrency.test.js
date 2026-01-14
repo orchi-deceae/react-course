abbb7e2cfb2608a65fb9887255fb73f8116ecd10
oksherlock <oksherlock9661@gmail.com>
2026-01-14 20:04:54 +0100

    0.47e - Created more tests for formatCurrency

--- ecommerce-project/src/components/utils/formatCurrency.test.js
+++ ecommerce-project/src/components/utils/formatCurrency.test.js
@@ -9,5 +9,12 @@ describe("formatCurrency", ()=>{
         expect(formatCurrency(1090)).toBe("10.90")
         expect(formatCurrency(100)).toBe("1.00")
     });
+    it("works with the number 0", ()=>{
+        expect(formatCurrency(0)).toBe("0.00")
+    });
+    it("works with negative numbers", ()=>{
+        expect(formatCurrency(-999)).toBe("-9.99")
+        expect(formatCurrency(-100)).toBe("-1.00")
+    });
 })
 // npx vitest
