3c9d1c512efa9bdaa60fe92bc369b0f391035ed4
oksherlock <oksherlock9661@gmail.com>
2026-01-14 17:18:18 +0100

    0.46.1a - Create unit test for formatCurrency

---
+++ ecommerce-project/src/components/utils/formatCurrency.test.js
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "vitest"
+import formatCurrency from "./formatCurrency"
+
+describe("formatCurrency", ()=>{
+    it("formats 1999 to $19.99", ()=>{
+        expect(formatCurrency(1999)).toBe("19.99");
+    });
+    it("displays 2 decimals", ()=>{
+        expect(formatCurrency(1090)).toBe("10.90")
+        expect(formatCurrency(10)).toBe("1.00")
+    });
+})
+// npx vitest
